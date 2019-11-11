b11384675603e7e9f54a143c4bfff85e66d31fc7
Anh Lu <anh@Anhs-MacBook-Pro-2.local>
2019-11-11 18:32:58 -0500

    added upload-to-erc.sh

---
+++ server/upload-to-ecr.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+aws ecr create-repository --repository-name flask-postgresdb-docker
+docker build -t flask-postgresdb-docker .
+docker tag flask-postgresdb-docker 278749303080.dkr.ecr.us-east-1.amazonaws.com/flask-postgresdb-docker:latest
+docker push 278749303080.dkr.ecr.us-east-1.amazonaws.com/flask-postgresdb-docker:latest
